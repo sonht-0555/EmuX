0d6b5fbeff5ec21b30ea297f4eb96e5b54f711ef
Son Hoang Tuan <hoang.tuan.son@sun-asterisk.com>
2026-03-02 20:21:47 +0700

    Update build-all-cores.yml

--- .github/workflows/retro/retro_glue.c
+++ .github/workflows/retro/retro_glue.c
@@ -19,20 +19,6 @@
 /* Weak attribute to avoid "multiple definition" errors when linking with cores that already have these functions */
 #define WEAK __attribute__((weak))
 
-/* CRC32 Utility */
-WEAK uint32_t encoding_crc32(uint32_t crc, const uint8_t *data, size_t len) {
-  return (uint32_t)crc32((unsigned long)crc, (const unsigned char *)data, (unsigned int)len);
-}
-
-WEAK char *string_trim_whitespace_right(char *str) {
-  if (!str) return NULL;
-  char *end = str + strlen(str) - 1;
-  while (end >= str && (*end == ' ' || *end == '\t' || *end == '\n' || *end == '\r')) {
-    *end = '\0';
-    end--;
-  }
-  return str;
-}
 
 WEAK size_t strlcpy_retro__(char *dest, const char *src, size_t size) {
   size_t i;
@@ -77,45 +63,6 @@ WEAK char *string_to_lower(const char *str) {
   return lower;
 }
 
-WEAK char *
-string_replace_substring(const char *in, size_t in_len, const char *pattern,
-                         size_t pattern_len, const char *replacement,
-                         size_t replacement_len) {
-  size_t outlen;
-  size_t numhits = 0;
-  const char *inat = NULL;
-  const char *inprev = NULL;
-  char *out = NULL;
-  char *outat = NULL;
-
-  if (!pattern || !replacement)
-    return strdup(in);
-
-  inat = in;
-  while ((inat = strstr(inat, pattern))) {
-    inat += pattern_len;
-    numhits++;
-  }
-
-  outlen = in_len - pattern_len * numhits + replacement_len * numhits;
-  if (!(out = (char *)malloc(outlen + 1)))
-    return NULL;
-
-  outat = out;
-  inat = in;
-  inprev = in;
-
-  while ((inat = strstr(inat, pattern))) {
-    memcpy(outat, inprev, inat - inprev);
-    outat += inat - inprev;
-    memcpy(outat, replacement, replacement_len);
-    outat += replacement_len;
-    inat += pattern_len;
-    inprev = inat;
-  }
-  strcpy(outat, inprev);
-  return out;
-}
 
 /* Path utilities */
 WEAK const char *find_last_slash(const char *str) {
@@ -123,10 +70,6 @@ WEAK const char *find_last_slash(const char *str) {
   return (s1 > s2) ? s1 : (s2 ? s2 : s1);
 }
 
-WEAK const char *path_basename(const char *path) {
-  const char *last = find_last_slash(path);
-  return last ? last + 1 : path;
-}
 
 WEAK void path_parent_dir(char *path) {
   char *last = (char *)find_last_slash(path);
@@ -171,20 +114,7 @@ WEAK void fill_pathname_resolve_relative(char *out, const char *dir, const char 
   }
 }
 
-WEAK const char *path_get_extension(const char *path) {
-  const char *dot = strrchr(path, '.');
-  const char *slash = find_last_slash(path);
-  return (dot && (!slash || dot > slash)) ? dot + 1 : "";
-}
 
-WEAK bool path_mkdir(const char *dir) {
-  return mkdir(dir, 0777) == 0;
-}
-
-WEAK bool path_is_directory(const char *path) {
-  struct stat st;
-  return (stat(path, &st) == 0 && S_ISDIR(st.st_mode));
-}
 
 /* Directory utilities */
 struct RDIR {
@@ -207,9 +137,6 @@ WEAK bool retro_readdir(struct RDIR *r) {
   return r && (r->e = readdir(r->d)) != NULL;
 }
 
-WEAK const char *retro_dirent_get_name(struct RDIR *r) {
-  return r && r->e ? r->e->d_name : NULL;
-}
 
 WEAK bool retro_dirent_is_dir(struct RDIR *r,
                                                const char *path) {
@@ -223,9 +150,6 @@ WEAK bool retro_dirent_is_dir(struct RDIR *r,
   return (stat(fp, &st) == 0 && S_ISDIR(st.st_mode));
 }
 
-WEAK int retro_dirent_error(struct RDIR *r) {
-  return 0;
-}
 
 WEAK void retro_closedir(struct RDIR *r) {
   if (r) {
@@ -274,81 +198,7 @@ WEAK int filestream_error(void *stream) {
 }
 
 WEAK void filestream_vfs_init(void) { }
-WEAK void dirent_vfs_init(void) { }
-WEAK void path_vfs_init(void) { }
 
-/* Memstream utilities for cores like Pokemon Mini */
-struct memstream {
-  uint8_t *buf;
-  size_t size;
-  size_t pos;
-  bool writable;
-};
-
-WEAK void *memstream_open(bool writable) {
-  struct memstream *m = (struct memstream *)calloc(1, sizeof(*m));
-  m->writable = writable;
-  return m;
-}
-
-WEAK void memstream_close(void *stream) {
-  free(stream);
-}
-
-WEAK void memstream_set_buffer(void *stream, uint8_t *buf, size_t size) {
-  struct memstream *m = (struct memstream *)stream;
-  if (!m) return;
-  m->buf = buf;
-  m->size = size;
-  m->pos = 0;
-}
-
-WEAK int64_t memstream_read(void *stream, void *data, int64_t len) {
-  struct memstream *m = (struct memstream *)stream;
-  if (!m || !m->buf || m->pos >= m->size) return 0;
-  if (m->pos + len > m->size) len = m->size - m->pos;
-  memcpy(data, m->buf + m->pos, (size_t)len);
-  m->pos += (size_t)len;
-  return len;
-}
-
-WEAK int64_t memstream_write(void *stream, const void *data, int64_t len) {
-  struct memstream *m = (struct memstream *)stream;
-  if (!m || !m->buf || !m->writable || m->pos >= m->size) return 0;
-  if (m->pos + len > m->size) len = m->size - m->pos;
-  memcpy(m->buf + m->pos, data, (size_t)len);
-  m->pos += (size_t)len;
-  return len;
-}
-
-WEAK int64_t memstream_seek(void *stream, int64_t offset, int whence) {
-  struct memstream *m = (struct memstream *)stream;
-  if (!m) return -1;
-  size_t new_pos = m->pos;
-  switch (whence) {
-    case SEEK_SET: new_pos = (size_t)offset; break;
-    case SEEK_CUR: new_pos += (size_t)offset; break;
-    case SEEK_END: new_pos = m->size + (size_t)offset; break;
-  }
-  if (new_pos > m->size) return -1;
-  m->pos = new_pos;
-  return (int64_t)m->pos;
-}
-
-WEAK int64_t memstream_tell(void *stream) {
-  return stream ? (int64_t)((struct memstream *)stream)->pos : -1;
-}
-
-WEAK uint64_t memstream_pos(void *stream) {
-  return stream ? (uint64_t)((struct memstream *)stream)->pos : 0;
-}
-
-WEAK uint8_t *memstream_get_ptr(void *stream, size_t *size) {
-  struct memstream *m = (struct memstream *)stream;
-  if (!m) return NULL;
-  if (size) *size = m->size;
-  return m->buf;
-}
 
 WEAK EMSCRIPTEN_KEEPALIVE int64_t filestream_seek(void *stream, int64_t offset,
                                               int seek_position) {
@@ -361,9 +211,6 @@ WEAK int64_t filestream_tell(void *stream) {
   return (int64_t)ftello((FILE *)stream);
 }
 
-WEAK void filestream_rewind(void *stream) {
-  rewind((FILE *)stream);
-}
 
 WEAK int filestream_close(void *stream) {
   return fclose((FILE *)stream);
@@ -391,10 +238,6 @@ WEAK int64_t filestream_read_file(const char *path, void **buf, int64_t *len) {
   return 1;
 }
 
-WEAK bool filestream_exists(const char *path) {
-  struct stat st;
-  return (stat(path, &st) == 0);
-}
 
 WEAK int64_t filestream_get_size(void *stream) {
   FILE *fp = (FILE *)stream;
@@ -407,45 +250,6 @@ WEAK int64_t filestream_get_size(void *stream) {
   return size;
 }
 
-/* Libretro Common Aliases */
-WEAK void *rfopen(const char *path, const char *mode) {
-  return (void *)fopen(path, mode);
-}
-WEAK int64_t rfread(void *buffer, size_t size, size_t count,
-                                     void *stream) {
-  return (int64_t)fread(buffer, size, count, (FILE *)stream);
-}
-WEAK int64_t rfwrite(const void *buffer, size_t size,
-                                      size_t count, void *stream) {
-  return (int64_t)fwrite(buffer, size, count, (FILE *)stream);
-}
-WEAK int64_t rfseek(void *stream, int64_t offset, int origin) {
-  return filestream_seek(stream, offset, origin);
-}
-WEAK int64_t rftell(void *stream) {
-  return (int64_t)ftello((FILE *)stream);
-}
-WEAK int64_t rfsize(void *stream) {
-  return filestream_get_size(stream);
-}
-WEAK char *rfgets(char *s, int len, void *stream) {
-  return fgets(s, len, (FILE *)stream);
-}
-WEAK int rfeof(void *stream) {
-  return feof((FILE *)stream);
-}
-WEAK int rfgetc(void *stream) {
-  return fgetc((FILE *)stream);
-}
-WEAK int rferror(void *stream) {
-  return ferror((FILE *)stream);
-}
-WEAK int rfclose(void *stream) {
-  return fclose((FILE *)stream);
-}
-WEAK int64_t rfget_size(void *stream) {
-  return filestream_get_size(stream);
-}
 
 /* VFS Callbacks Wrapper */
 struct retro_vfs_file_handle {
--- .github/workflows/retro/retro_neogeo.c
+++ .github/workflows/retro/retro_neogeo.c
@@ -18,11 +18,6 @@
 /* Weak attribute to avoid "multiple definition" errors when linking with cores that already have these functions */
 #define WEAK __attribute__((weak))
 
-/* Stubs to fix Emscripten LTO linker bug */
-#ifdef __EMSCRIPTEN__
-WEAK EMSCRIPTEN_KEEPALIVE void* __get_tp() { return 0; }
-WEAK EMSCRIPTEN_KEEPALIVE void emscripten_futex_wake() { }
-#endif
 
 /* Libretro VFS Constants */
 #define RETRO_VFS_FILE_ACCESS_READ (1 << 0)
@@ -45,45 +40,6 @@ WEAK EMSCRIPTEN_KEEPALIVE char *string_to_lower(const char *str) {
   return lower;
 }
 
-WEAK EMSCRIPTEN_KEEPALIVE char *
-string_replace_substring(const char *in, size_t in_len, const char *pattern,
-                         size_t pattern_len, const char *replacement,
-                         size_t replacement_len) {
-  size_t outlen;
-  size_t numhits = 0;
-  const char *inat = NULL;
-  const char *inprev = NULL;
-  char *out = NULL;
-  char *outat = NULL;
-
-  if (!pattern || !replacement)
-    return strdup(in);
-
-  inat = in;
-  while ((inat = strstr(inat, pattern))) {
-    inat += pattern_len;
-    numhits++;
-  }
-
-  outlen = in_len - pattern_len * numhits + replacement_len * numhits;
-  if (!(out = (char *)malloc(outlen + 1)))
-    return NULL;
-
-  outat = out;
-  inat = in;
-  inprev = in;
-
-  while ((inat = strstr(inat, pattern))) {
-    memcpy(outat, inprev, inat - inprev);
-    outat += inat - inprev;
-    memcpy(outat, replacement, replacement_len);
-    outat += replacement_len;
-    inat += pattern_len;
-    inprev = inat;
-  }
-  strcpy(outat, inprev);
-  return out;
-}
 
 /* Path utilities */
 WEAK EMSCRIPTEN_KEEPALIVE const char *find_last_slash(const char *str) {
@@ -91,10 +47,6 @@ WEAK EMSCRIPTEN_KEEPALIVE const char *find_last_slash(const char *str) {
   return (s1 > s2) ? s1 : (s2 ? s2 : s1);
 }
 
-WEAK EMSCRIPTEN_KEEPALIVE const char *path_basename(const char *path) {
-  const char *last = find_last_slash(path);
-  return last ? last + 1 : path;
-}
 
 WEAK EMSCRIPTEN_KEEPALIVE void path_parent_dir(char *path) {
   char *last = (char *)find_last_slash(path);
@@ -104,32 +56,7 @@ WEAK EMSCRIPTEN_KEEPALIVE void path_parent_dir(char *path) {
     *path = '\0';
 }
 
-WEAK EMSCRIPTEN_KEEPALIVE bool path_is_absolute(const char *path) {
-  return (path && (path[0] == '/' ||
-                   (strlen(path) > 2 && path[1] == ':' && path[2] == '\\')));
-}
 
-WEAK EMSCRIPTEN_KEEPALIVE void path_remove_extension(char *path) {
-  char *dot = strrchr(path, '.');
-  const char *slash = find_last_slash(path);
-  if (dot && (!slash || dot > slash))
-    *dot = '\0';
-}
-
-WEAK EMSCRIPTEN_KEEPALIVE const char *path_get_extension(const char *path) {
-  const char *dot = strrchr(path, '.');
-  const char *slash = find_last_slash(path);
-  return (dot && (!slash || dot > slash)) ? dot + 1 : "";
-}
-
-WEAK EMSCRIPTEN_KEEPALIVE bool path_mkdir(const char *dir) {
-  return mkdir(dir, 0777) == 0;
-}
-
-WEAK EMSCRIPTEN_KEEPALIVE bool path_is_directory(const char *path) {
-  struct stat st;
-  return (stat(path, &st) == 0 && S_ISDIR(st.st_mode));
-}
 
 /* Directory utilities */
 struct RDIR {
@@ -152,9 +79,6 @@ WEAK EMSCRIPTEN_KEEPALIVE bool retro_readdir(struct RDIR *r) {
   return r && (r->e = readdir(r->d)) != NULL;
 }
 
-WEAK EMSCRIPTEN_KEEPALIVE const char *retro_dirent_get_name(struct RDIR *r) {
-  return r && r->e ? r->e->d_name : NULL;
-}
 
 WEAK EMSCRIPTEN_KEEPALIVE bool retro_dirent_is_dir(struct RDIR *r,
                                               const char *path) {
@@ -217,16 +141,6 @@ WEAK EMSCRIPTEN_KEEPALIVE int filestream_close(void *stream) {
   return fclose((FILE *)stream);
 }
 
-WEAK EMSCRIPTEN_KEEPALIVE int64_t filestream_get_size(void *stream) {
-  FILE *fp = (FILE *)stream;
-  if (!fp)
-    return 0;
-  int64_t curr = ftello(fp);
-  fseeko(fp, 0, SEEK_END);
-  int64_t size = ftello(fp);
-  fseeko(fp, curr, SEEK_SET);
-  return size;
-}
 
 /* Libretro Common Aliases */
 WEAK EMSCRIPTEN_KEEPALIVE void *rfopen(const char *path, const char *mode) {
@@ -246,14 +160,19 @@ WEAK EMSCRIPTEN_KEEPALIVE int64_t rfseek(void *stream, int64_t offset, int origi
 WEAK EMSCRIPTEN_KEEPALIVE int64_t rftell(void *stream) {
   return (int64_t)ftello((FILE *)stream);
 }
-WEAK EMSCRIPTEN_KEEPALIVE int64_t rfsize(void *stream) {
-  return filestream_get_size(stream);
-}
 WEAK EMSCRIPTEN_KEEPALIVE int rfclose(void *stream) {
   return fclose((FILE *)stream);
 }
-WEAK EMSCRIPTEN_KEEPALIVE int64_t rfget_size(void *stream) {
-  return filestream_get_size(stream);
+
+WEAK EMSCRIPTEN_KEEPALIVE int64_t filestream_get_size(void *stream) {
+  FILE *fp = (FILE *)stream;
+  if (!fp)
+    return 0;
+  int64_t curr = ftello(fp);
+  fseeko(fp, 0, SEEK_END);
+  int64_t size = ftello(fp);
+  fseeko(fp, curr, SEEK_SET);
+  return size;
 }
 
 /* VFS Callbacks Wrapper */
